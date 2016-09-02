f329ddf8a877398bf18f5ca0439557de91313408
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 16:33:48 +0200

    Add caf.mk
    * Packages and apns list

---
+++ caf.mk
@@ -0,0 +1,13 @@
+PRODUCT_PACKAGES += \
+    AccountAndSyncSettings \
+    BluetoothExt \
+    DrmProvider \
+    SnapdragonGallery \
+    SnapdragonLauncher \
+    SnapdragonMusic \
+    Launcher3 \
+    LatinIME \
+    messaging
+
+PRODUCT_COPY_FILES += \
+    caf_manifest/apns-conf.xml:system/etc/apns-conf.xml
