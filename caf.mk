a7e18baefd9982771d7a6b8d13e217f77f0048b9
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-05 15:44:13 +0200

    Update caf.mk

--- caf.mk
+++ caf.mk
@@ -6,19 +6,49 @@ PRODUCT_PACKAGES += \
     SnapdragonCamera \
     Launcher3 \
     LatinIME \
-    messaging \
-    Stk
+    messaging
 
 # Utils
 PRODUCT_PACKAGES += \
     curl \
     powertop
 
-# Prebuilt
+# World APN list
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files, *, caf_manifest/prebuilt, system)
+    caf_manifest/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# Props
+ALARM_PATH := caf_manifest/prebuilt/media/audio/alarms
+NOTIFICATION_PATH := caf_manifest/prebuilt/media/audio/notifications
+RINGTONE_PATH := caf_manifest/prebuilt/media/audio/ringtones
+UI_PATH := caf_manifest/prebuilt/media/audio/ui
+
+# Alarms
+PRODUCT_COPY_FILES += \
+    $(ALARM_PATH)/CyanAlarm.ogg:system/media/audio/alarms/CyanAlarm.ogg \
+    $(ALARM_PATH)/NuclearLaunch.ogg:system/media/audio/alarms/NuclearLaunch.ogg
+
+# Notifications
+PRODUCT_COPY_FILES += \
+    $(NOTIFICATION_PATH)/CyanDoink.ogg:system/media/audio/notifications/CyanDoink.ogg \
+    $(NOTIFICATION_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
+    $(NOTIFICATION_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
+    $(NOTIFICATION_PATH)/Laser.ogg:system/media/audio/notifications/Laser.ogg \
+    $(NOTIFICATION_PATH)/Naughty.ogg:system/media/audio/notifications/Naughty.ogg \
+    $(NOTIFICATION_PATH)/Pong.ogg:system/media/audio/notifications/Pong.ogg \
+    $(NOTIFICATION_PATH)/Rang.ogg:system/media/audio/notifications/Rang.ogg \
+    $(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/Stone.ogg
+
+# Ringtones
+PRODUCT_COPY_FILES += \
+    $(RINGTONE_PATH)/Boxbeat.ogg:system/media/audio/ringtones/Boxbeat.ogg \
+    $(RINGTONE_PATH)/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg \
+    $(RINGTONE_PATH)/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
+    $(RINGTONE_PATH)/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
+    $(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
+    $(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3 \
+    $(RINGTONE_PATH)/Yukaay.ogg:system/media/audio/ringtones/Yukaay.ogg
+
+# GSM
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google
