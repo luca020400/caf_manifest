2c0b7dfa74b8c75973b742a400363c11323aeb67
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-05 12:26:06 +0200

    caf: Add some props

--- caf.mk
+++ caf.mk
@@ -1,3 +1,4 @@
+# Packages
 PRODUCT_PACKAGES += \
     BluetoothExt \
     SnapdragonGallery \
@@ -5,8 +6,31 @@ PRODUCT_PACKAGES += \
     SnapdragonCamera \
     Launcher3 \
     LatinIME \
-    messaging
+    messaging \
+    Stk
+
+# Utils
+PRODUCT_PACKAGES += \
+    curl \
+    powertop
 
 # Prebuilt
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files, *, caf_manifest/prebuilt, system)
+
+# Props
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false
