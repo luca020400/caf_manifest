49d09eacc04b3416d746257b1a408d9645c845a8
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-07 12:57:51 +0200

    get ready for more security

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,13 @@
-if [ -d "caf_manifest/patches_security" ]; then
-    sh "caf_manifest/patches_security/apply.sh"
-fi
+months=("january february march april may june july august september october november december")
+years=("2016 2017")
+
+for year in ${years[@]}; do
+    for month in ${months[@]}; do
+        if [ -f "caf_manifest/patches_security/$year/$month/apply.sh" ]; then
+            sh "caf_manifest/patches_security/$year/$month/apply.sh"
+        fi
+    done
+done
 
 sh caf_manifest/patches_custom/apply.sh
 
