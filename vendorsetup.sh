35dc5aa36d924ed8c596714d2b2d895a2017c68d
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-07 10:26:32 +0200

    prefer security over our patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
-sh caf_manifest/patches/apply.sh
-
 if [ -d "caf_manifest/patches_security" ]; then
     sh "caf_manifest/patches_security/apply.sh"
 fi
+
+sh caf_manifest/patches/apply_custom.sh
 
 if [ -d "caf_manifest/patches_$USER" ]; then
     sh "caf_manifest/patches_$USER/apply.sh"
