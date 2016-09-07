26333c23e7b0dbbda88f660c03a13b12c45e4713
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-07 10:27:47 +0200

    Fix typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ if [ -d "caf_manifest/patches_security" ]; then
     sh "caf_manifest/patches_security/apply.sh"
 fi
 
-sh caf_manifest/patches/apply_custom.sh
+sh caf_manifest/patches_custom/apply.sh
 
 if [ -d "caf_manifest/patches_$USER" ]; then
     sh "caf_manifest/patches_$USER/apply.sh"
