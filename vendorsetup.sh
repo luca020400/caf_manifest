7b4799691d22d9514ec926bfb06533d8e324a6a3
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-07 10:16:01 +0200

    caf_manifest: Merge android-7.0.0_r4 changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,9 @@
 sh caf_manifest/patches/apply.sh
 
+if [ -d "caf_manifest/patches_security" ]; then
+    sh "caf_manifest/patches_security/apply.sh"
+fi
+
 if [ -d "caf_manifest/patches_$USER" ]; then
     sh "caf_manifest/patches_$USER/apply.sh"
 fi
